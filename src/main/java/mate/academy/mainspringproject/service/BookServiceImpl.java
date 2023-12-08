2a56b8699974609f9e75751fdb3a40469233cd4b
OLEH <avanesovob@gmail.com>
2023-12-08 15:40:32 +0200

    Used @RequiredArgsConstructor for BookRepositoryImpl and BookServiceImpl

--- src/main/java/mate/academy/mainspringproject/service/BookServiceImpl.java
+++ src/main/java/mate/academy/mainspringproject/service/BookServiceImpl.java
@@ -1,25 +1,20 @@
 package mate.academy.mainspringproject.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.mainspringproject.model.Book;
 import mate.academy.mainspringproject.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     @Override
     public Book save(Book book) {
-        bookRepository.save(book);
-        return book;
+        return bookRepository.save(book);
     }
 
     @Override
