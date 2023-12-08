110e484c5417d0dc3f6f2d24c04eabfd0c28bc38
OLEH <avanesovob@gmail.com>
2023-12-08 12:17:32 +0200

    first commit

---
+++ src/main/java/mate/academy/mainspringproject/MainSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.mainspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/academy/mainspringproject/MainSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.mainspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
