33e76142f2b59532396710fc9fa2bdf03d72e6ea
OLEH <avanesovob@gmail.com>
2023-12-26 20:32:32 +0200

    Used JpaRepository, add soft delete, add update and delete methods, used Liquibase

--- src/main/java/mate/academy/mainspringproject/service/BookServiceImpl.java
+++ src/main/java/mate/academy/mainspringproject/service/BookServiceImpl.java
@@ -45,15 +45,15 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto updateById(Long id, CreateBookRequestDto requestDto) {
-            Book book = bookRepository.findById(id).orElseThrow(
-                    () -> new EntityNotFoundException("Can't find book by id: " + id));
-            book.setId(id);
-            book.setTitle(requestDto.getTitle());
-            book.setAuthor(requestDto.getAuthor());
-            book.setIsbn(requestDto.getIsbn());
-            book.setPrice(requestDto.getPrice());
-            book.setDescription(requestDto.getDescription());
-            book.setCoverImage(requestDto.getCoverImage());
-            return bookMapper.toDto(bookRepository.save(book));
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id: " + id));
+        book.setId(id);
+        book.setTitle(requestDto.getTitle());
+        book.setAuthor(requestDto.getAuthor());
+        book.setIsbn(requestDto.getIsbn());
+        book.setPrice(requestDto.getPrice());
+        book.setDescription(requestDto.getDescription());
+        book.setCoverImage(requestDto.getCoverImage());
+        return bookMapper.toDto(bookRepository.save(book));
     }
 }
