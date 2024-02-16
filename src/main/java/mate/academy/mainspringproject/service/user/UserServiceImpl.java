960929a34a90dc8a75d5747b0ddc5d25fb7513a7
OLEH <avanesovob@gmail.com>
2024-02-16 16:00:46 +0200

    Added encode

--- src/main/java/mate/academy/mainspringproject/service/user/UserServiceImpl.java
+++ src/main/java/mate/academy/mainspringproject/service/user/UserServiceImpl.java
@@ -6,6 +6,7 @@ import mate.academy.mainspringproject.dto.user.UserResponseDto;
 import mate.academy.mainspringproject.mappers.UserMapper;
 import mate.academy.mainspringproject.model.User;
 import mate.academy.mainspringproject.repository.user.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +14,13 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto save(UserRegistrationRequestDto requestDto) {
         User userForRegistration = userMapper.toModel(requestDto);
+        String encodedPassword = passwordEncoder.encode(userForRegistration.getPassword());
+        userForRegistration.setPassword(encodedPassword);
         return userMapper.toDto(userRepository.save(userForRegistration));
     }
 }
