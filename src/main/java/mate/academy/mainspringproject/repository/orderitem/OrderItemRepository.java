4689ace66dd4c1b2f519690d88bb77675662b818
OLEH <avanesovob@gmail.com>
2024-03-13 20:05:43 +0200

    Added Order model(revert)

--- src/main/java/mate/academy/mainspringproject/repository/orderitem/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.mainspringproject.repository.orderitem;
-
-import mate.academy.mainspringproject.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
