c139dd3babbefd6e2c4cf0913449812c4e8f5a62
OLEH <avanesovob@gmail.com>
2024-03-22 20:39:03 +0200

    Added Docker to the project

--- src/test/java/mate/academy/mainspringproject/MainSpringProjectApplicationTests.java
+++ src/test/java/mate/academy/mainspringproject/MainSpringProjectApplicationTests.java
@@ -1,13 +1,8 @@
 package mate.academy.mainspringproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainSpringProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
